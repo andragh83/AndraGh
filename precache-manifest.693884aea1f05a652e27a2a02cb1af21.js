1b0058c6b06b35b458186a683cacc793e9fac80c
Andra Ghitulescu <andra.ghitulescu@gmail.com>
2019-04-25 00:56:58 +0300

    Updates

---
+++ precache-manifest.693884aea1f05a652e27a2a02cb1af21.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e87e6e4224aa6e267e4dad930a4203b",
+    "url": "/AndraGh/static/media/art5.0e87e6e4.png"
+  },
+  {
+    "revision": "79d011f7f12636bae499",
+    "url": "/AndraGh/static/css/main.c8374775.chunk.css"
+  },
+  {
+    "revision": "8c2c229837b720e0ba62",
+    "url": "/AndraGh/static/js/runtime~main.8c2c2298.js"
+  },
+  {
+    "revision": "018d1b93dfff6479da4b",
+    "url": "/AndraGh/static/js/2.018d1b93.chunk.js"
+  },
+  {
+    "revision": "87fb2d3bc417667e46f25a737b8d6964",
+    "url": "/AndraGh/static/media/logo_agh_black.87fb2d3b.svg"
+  },
+  {
+    "revision": "c7c60e67e475c22037c9c058c5af77a0",
+    "url": "/AndraGh/static/media/Illustration.c7c60e67.svg"
+  },
+  {
+    "revision": "739d7b28d6d4e7f7a23638b1f9b4e30b",
+    "url": "/AndraGh/static/media/GD.739d7b28.svg"
+  },
+  {
+    "revision": "da0b029d4f768ff234dd7dca2d703371",
+    "url": "/AndraGh/static/media/Websites.da0b029d.svg"
+  },
+  {
+    "revision": "08a175f6bc3c2798ca0ca686599c3f72",
+    "url": "/AndraGh/static/media/IllustrationTrinity.08a175f6.png"
+  },
+  {
+    "revision": "377ca6cf03d1fbc2031ce9ef4b3ee30c",
+    "url": "/AndraGh/static/media/trad1.377ca6cf.png"
+  },
+  {
+    "revision": "59630651d6bd2162153c5827342ccd02",
+    "url": "/AndraGh/static/media/trad2.59630651.png"
+  },
+  {
+    "revision": "a12dc16a418b40c059f5614ff9ccacda",
+    "url": "/AndraGh/static/media/trad3.a12dc16a.png"
+  },
+  {
+    "revision": "03d27e8f3d3e0051ad403b6432b57f89",
+    "url": "/AndraGh/static/media/trad4.03d27e8f.png"
+  },
+  {
+    "revision": "22dc5b822bbd5501dc5626bdf2124f2d",
+    "url": "/AndraGh/static/media/trad5.22dc5b82.png"
+  },
+  {
+    "revision": "cb99ac5232b096501f98e41e5839656a",
+    "url": "/AndraGh/static/media/digital1.cb99ac52.jpg"
+  },
+  {
+    "revision": "d9eebb0af1e9da29e3bf4e1a61769969",
+    "url": "/AndraGh/static/media/digital2.d9eebb0a.jpg"
+  },
+  {
+    "revision": "44ed1e8e00076258fb26c19cb1425608",
+    "url": "/AndraGh/static/media/digital3.44ed1e8e.jpg"
+  },
+  {
+    "revision": "356231c614e9951c47d0b8ea67092fc1",
+    "url": "/AndraGh/static/media/digital4.356231c6.png"
+  },
+  {
+    "revision": "6a8b308da567cb9553da6cdaebe70143",
+    "url": "/AndraGh/static/media/digital5.6a8b308d.png"
+  },
+  {
+    "revision": "98df375784b5686fe47eb212cb0ff73d",
+    "url": "/AndraGh/static/media/art1.98df3757.png"
+  },
+  {
+    "revision": "1177a5d6f5d633576632fd54f6c50c84",
+    "url": "/AndraGh/static/media/art2.1177a5d6.png"
+  },
+  {
+    "revision": "65b0d8e3b8bbb18602a76aefa5290e23",
+    "url": "/AndraGh/static/media/art3.65b0d8e3.png"
+  },
+  {
+    "revision": "3b3265f5e732d10d62778619f3085ea8",
+    "url": "/AndraGh/static/media/art4.3b3265f5.png"
+  },
+  {
+    "revision": "79d011f7f12636bae499",
+    "url": "/AndraGh/static/js/main.79d011f7.chunk.js"
+  },
+  {
+    "revision": "7144e0e8fef24b30ff1970ae97a9d6f0",
+    "url": "/AndraGh/static/media/Webtrinity.7144e0e8.png"
+  },
+  {
+    "revision": "58bbc576e0a931b077970d41dbdefa47",
+    "url": "/AndraGh/static/media/Websites1.58bbc576.png"
+  },
+  {
+    "revision": "4a360b811447da0196298dcef6a1a445",
+    "url": "/AndraGh/static/media/Websites2.4a360b81.png"
+  },
+  {
+    "revision": "88901ff96da9940e33ca76ba0cb0869b",
+    "url": "/AndraGh/static/media/Websites3.88901ff9.png"
+  },
+  {
+    "revision": "4ad12a49411333f3e82ab7472245c7c8",
+    "url": "/AndraGh/static/media/Websites4.4ad12a49.png"
+  },
+  {
+    "revision": "60fabaa51460694d09a2f2b0973ac1a8",
+    "url": "/AndraGh/static/media/GDtrinity.60fabaa5.png"
+  },
+  {
+    "revision": "fc5769fbef9a915afbb67f7dcfccba30",
+    "url": "/AndraGh/static/media/logo1.fc5769fb.png"
+  },
+  {
+    "revision": "48be92a592471bde701b61277674574b",
+    "url": "/AndraGh/static/media/logo2.48be92a5.png"
+  },
+  {
+    "revision": "7be82ed3756004d86ebb7210d5023874",
+    "url": "/AndraGh/static/media/logo3.7be82ed3.png"
+  },
+  {
+    "revision": "4da1d54c6c864771e9f70d280a0e6188",
+    "url": "/AndraGh/static/media/promo1.4da1d54c.png"
+  },
+  {
+    "revision": "aa58cec9078dff7714e612d2861ef553",
+    "url": "/AndraGh/static/media/promo2.aa58cec9.png"
+  },
+  {
+    "revision": "e19e3c457f9db4d2170f11599e79bf08",
+    "url": "/AndraGh/static/media/promo3.e19e3c45.jpg"
+  },
+  {
+    "revision": "df1908ef1d8aa387c61e7fa1b2617ed5",
+    "url": "/AndraGh/static/media/planning1.df1908ef.jpg"
+  },
+  {
+    "revision": "b8bc513d22cd10fba1670e4a378925be",
+    "url": "/AndraGh/static/media/planning2.b8bc513d.jpg"
+  },
+  {
+    "revision": "b5296c09fa3ec4f439aa6684723b10a3",
+    "url": "/AndraGh/static/media/planning3.b5296c09.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/AndraGh/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "6bc10362c2fef9ef3f5cd554617b1ab8",
+    "url": "/AndraGh/static/media/fb.6bc10362.png"
+  },
+  {
+    "revision": "cd6db2c5f3f10484a2affd5d6e402ccd",
+    "url": "/AndraGh/static/media/insta.cd6db2c5.png"
+  },
+  {
+    "revision": "d8c29679add8ba2922a2f6ee2359b44d",
+    "url": "/AndraGh/static/media/twitter.d8c29679.png"
+  },
+  {
+    "revision": "5f8289d7b7750e357a769e9f29376be7",
+    "url": "/AndraGh/static/media/separator.5f8289d7.svg"
+  },
+  {
+    "revision": "40a731b78fbf2484e5bdf00922acc4e0",
+    "url": "/AndraGh/static/media/avatar.40a731b7.jpg"
+  },
+  {
+    "revision": "7e5004e65f86639a8353ee7f9765888d",
+    "url": "/AndraGh/index.html"
+  }
+];
